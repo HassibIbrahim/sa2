1f0c119ac8d07c207beadd9a246d3a500b8b8521
Hassib Ibrahim <ihassib@gmail.comclearcd cleargit -lgit --lostgit config --global user.email ihassib@gmail.com>
2018-09-06 01:32:28 +0300

    model on the front end css

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,11 @@ $(document).ready(function(){
     $("p.display").text("Successfully Subscribed")
   })
 })
+
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
